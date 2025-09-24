e4bf2c9a170c3a6ad7c0832d7b4404e5f7d9bdb7
phantomchzo-ui <224369715+phantomchzo-ui@users.noreply.github.com>
2025-09-24 17:08:41 +0500

    validation password

--- app/users/schemas.py
+++ app/users/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, EmailStr, constr, Field
+import re
+
+from pydantic import BaseModel, EmailStr, constr, field_validator
 
 PasswordType = constr(min_length=6)
 
@@ -7,9 +9,24 @@ class SUserSchemas(BaseModel):
     name: str
     hashed_password: str
 
+    @field_validator('hashed_password')
+    def validate_password(cls, v: str):
+        if len(v) < 6:
+            raise ValueError('Пароль должен содержать минимум 6 символов')
+        if not re.search(r"[A-Z]", v):
+            raise ValueError('Пароль должен содержать хотя бы одну заглавную букву')
+        if not re.search(r"[a-z]", v):
+            raise ValueError("Пароль должен содержать хотя бы одну строчную букву")
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", v):
+            raise ValueError("Пароль должен содержать хотя бы один спецсимвол")
+        return v
+
+
 class SUserLoginSchemas(BaseModel):
     email: EmailStr
-    password: PasswordType
+    password: str
+
+
 
 class SUserSchemasUpdate(BaseModel):
     email: EmailStr
