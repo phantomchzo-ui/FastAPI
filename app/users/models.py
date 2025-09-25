9f5a8815547f20df0b3260cf63b61e7a65919287
phantomchzo-ui <224369715+phantomchzo-ui@users.noreply.github.com>
2025-09-25 16:31:06 +0500

    add balance column in users table + migration

--- app/users/models.py
+++ app/users/models.py
@@ -14,6 +14,7 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(25))
     email: Mapped[EmailStr] = mapped_column(String(50))
     hashed_password: Mapped[str]
+    balance:Mapped[int] = mapped_column(default=0)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
