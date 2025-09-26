6363db65807e672bbf0478e6b4711bf5d51b6506
phantomchzo-ui <224369715+phantomchzo-ui@users.noreply.github.com>
2025-09-26 19:34:06 +0500

    add celery_app and new conf

--- app/config.py
+++ app/config.py
@@ -11,6 +11,11 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
 
+    GM_HOST: str
+    GM_PORT: int
+    GM_USER: str
+    GM_PASSWORD: str
+
     @property
     def DATABASE_URL(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
