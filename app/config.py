945c89870274041bb7b50da9694f243cbfbdf1e9
phantomchzo-ui <224369715+phantomchzo-ui@users.noreply.github.com>
2025-10-08 17:37:07 +0500

    change database_url

--- app/config.py
+++ app/config.py
@@ -8,6 +8,14 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
+
+    @property
+    def database_url(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    @property
+    def test_database_url(self):
+        return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASSWORD}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
+
     SECRET_KEY: str
     ALGORITHM: str
 
--- app/migrations/env.py
+++ app/migrations/env.py
@@ -21,7 +21,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL + "?async_fallback=True")
+config.set_main_option("sqlalchemy.url", settings.database_url + "?async_fallback=True")
 
 def run_migrations_offline() -> None:
     url = config.get_main_option("sqlalchemy.url")
