fbee05bfeabc55c83092420b4efa4a9b529b4c92
phantomchzo-ui <224369715+phantomchzo-ui@users.noreply.github.com>
2025-10-08 18:30:45 +0500

    api test users

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -8,6 +8,10 @@ from app.users.models import User
 from app.catalog.model import Catalog
 from app.products.model import Product
 
+from app.main import app as fastapi_app
+from fastapi.testclient import TestClient
+from httpx import AsyncClient, ASGITransport
+
 @pytest.fixture(scope="session")
 def event_loop():
     loop = asyncio.new_event_loop()
@@ -22,8 +26,15 @@ async def prepare_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
-        print("✅ Tables created!")
+
 
     yield
     #async with engine.begin() as conn:
         #await conn.run_sync(Base.metadata.drop_all)
+
+
+@pytest.fixture(scope="function")
+async def ac():
+    transport = ASGITransport(app=fastapi_app)
+    async with AsyncClient(transport=transport, base_url="http://test") as ac:
+        yield ac
--- app/tests/unit_test/test_users/test_api.py
+++ app/tests/unit_test/test_users/test_api.py
@@ -1,2 +1,28 @@
-def test_abc():
-    assert 1==1
+import pytest
+from httpx import AsyncClient
+
+
+@pytest.mark.parametrize("email, name, password, status_code", [
+    ("kot@pes.com", "Swaga", "ElamaN200409@", 200),
+    ("kot@pes.com", "Swaga", "ElamaN200409@", 409)
+])
+async def test_register(email, name, password, status_code, ac: AsyncClient):
+    response = await ac.post('/user/register', json={
+        "email": email,
+        "name": name,
+        "hashed_password": password
+    })
+
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ("kot@pes.com", "ElamaN200409@", 200),
+])
+async def test_login(email, password, status_code, ac: AsyncClient):
+    response = await ac.post('/user/login', json={
+        "email": email,
+        "password": password
+    })
+
+    assert response.status_code == status_code
