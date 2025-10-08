4b9186bb9c63dbc0365527a694fef9770f7159fb
phantomchzo-ui <224369715+phantomchzo-ui@users.noreply.github.com>
2025-10-08 17:36:19 +0500

    add pytest

---
+++ app/tests/conftest.py
@@ -0,0 +1,29 @@
+import asyncio
+import pytest
+
+from app.config import settings
+from app.database import Base, engine, async_session
+
+from app.users.models import User
+from app.catalog.model import Catalog
+from app.products.model import Product
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest.fixture(autouse=True, scope="session")
+async def prepare_database():
+    assert settings.MODE == "TEST", "Запуск тестов возможен только в режиме TEST!"
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+        print("✅ Tables created!")
+
+    yield
+    #async with engine.begin() as conn:
+        #await conn.run_sync(Base.metadata.drop_all)
